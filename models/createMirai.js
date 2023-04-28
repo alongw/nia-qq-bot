b34b9623c4c3a3f6e322af6d6a9a29249b1433e1
alongw <alongw@alongw.cn>
2023-04-28 22:34:49 +0800

    优化代码

--- models/createMirai.js
+++
@@ -1,7 +0,0 @@
-// createMirai 用于创建mirai实例，向外暴露相关功能
-import { CreateMiraiApi } from 'typescript-mirai-api-http';
-// 读取配置文件
-import configfile from './getconfig.js';
-const config = configfile;
-const { onMessage, onGroupMessage, onFriendMessage, sendGroupMessage, sendFriendMessage } = CreateMiraiApi(config.ip, config.port, config.key, config.qq);
-export { onMessage, onGroupMessage, onFriendMessage, sendGroupMessage, sendFriendMessage };
