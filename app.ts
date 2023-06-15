ad7ec5dcce007277ae12dd0c7323d25f213decd5
alongw <alongw@alongw.cn>
2023-06-15 18:00:35 +0800

    🎉 init: 重构项目

--- app.ts
+++
@@ -1,7 +0,0 @@
-// 读取配置文件
-import configfile from './models/getconfig.js'
-const config = configfile as { [key: string]: any };
-
-console.log(`niabot启动成功！\n当前qq号：${config.qq}\nMirai Http连接地址：ws://${config.ip}:${config.port}\nkey：***`);
-
-import './models/message.js'
